e7f4ba11862d3889327342acc9058b1a0c978b85
Robert Palmer <hi@robertpalmer.de>
2010-12-20 13:56:43 +0100

    real jsonp

--- hoccer_proxy.rb
+++ hoccer_proxy.rb
@@ -3,15 +3,13 @@ require 'sinatra'
 get %r{/clients/([a-zA-Z0-9\-]{36,36})/environment.js} do
   puts params.inspect
 
-  ""
+  "#{params['jsonp']}({content: \"hallo\"})"
 end
 
 get %r{/clients/([a-zA-Z0-9\-]{36,36})/action/send.js} do 
-  puts params.inspect
-
-  "linccer.onSend()"
+  "#{params['jsonp']}({content: \"hallo\"})"
 end
 
 get %r{/clients/([a-zA-Z0-9\-]{36,36})/action/receive\.js} do
-  "linccer.onReceived({content: \"hallo\"})"
+  "#{params['jsonp']}({content: \"hallo\"})"  
 end

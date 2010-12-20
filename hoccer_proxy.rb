929b8d93f9c34c67b27964ae4ae2c31eea4d3698
Robert Palmer <hi@robertpalmer.de>
2010-12-20 11:49:44 +0100

    added a pseudo uuid generator

--- hoccer_proxy.rb
+++ hoccer_proxy.rb
@@ -1,19 +1,17 @@
 require 'sinatra'
 
-get '/environment.js' do
+get %r{/clients/([a-zA-Z0-9\-]{36,36})/environment.js} do
   puts params.inspect
 
   ""
 end
 
-get "/send.js" do 
+get %r{/clients/([a-zA-Z0-9\-]{36,36})/action/send.js} do 
   puts params.inspect
 
   "linccer.onSend()"
 end
 
-get "/reveive.js" do
-  puts params.inspect
-
+get %r{/clients/([a-zA-Z0-9\-]{36,36})/action/receive\.js} do
   "linccer.onReceived({content: \"hallo\"})"
 end
